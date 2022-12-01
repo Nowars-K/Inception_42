a7a8d1edc34bbafd40ba97e552af165cf3c3b457
Alexsan <a.ferrand69@gmail.com>
2022-12-01 17:33:02 +0100

    love

--- srcs/requierements/mariadb/srcs/start.sh
+++ srcs/requierements/mariadb/srcs/start.sh
@@ -1,3 +1,13 @@
 /usr/bin/mysqld_safe &
 
+while ! nc -vz localhost 3306; do sleep 1; done
+
+/usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+/usr/bin/mysql -uroot -e "GRANT ALL ON $DB_DATABASE .* TO '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+/usr/bin/mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+
+while nc -vz localhost 3306; do sleep 1; done
+
 exec /usr/sbin/mysqld
