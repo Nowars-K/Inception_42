79cd0b7a625bca744aafd1fa049a231ecea68675
Alexsan <a.ferrand69@gmail.com>
2022-12-01 16:02:55 +0100

    info

--- srcs/requierements/mariadb/srcs/mariadb_setup.sh
+++ srcs/requierements/mariadb/srcs/mariadb_setup.sh
@@ -4,3 +4,4 @@ mv /my.cnf /etc/mysql/
 /etc/init.d/mysql start
 
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOTPASWORD';"
