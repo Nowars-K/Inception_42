2bcdbee1d61523d8b58959631972a390ff024744
Alexsan <a.ferrand69@gmail.com>
2022-12-07 11:08:27 +0100

    all

--- srcs/requierements/wordpress/srcs/wordpress_setup.sh
+++ srcs/requierements/wordpress/srcs/wordpress_setup.sh
@@ -7,10 +7,10 @@ mv wordpress/ /var/www/app
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/app
-sed -i "s/username_here/${MYSQL_ROOT_USR}/g" wp-config-sample.php
-sed -i "s/password_here/${MYSQL_USR_PWD}/g" wp-config-sample.php
+sed -i "s/username_here/aleferra/g" wp-config-sample.php
+sed -i "s/password_here/pass/g" wp-config-sample.php
 sed -i "s/localhost/mariadb/g" wp-config-sample.php
-sed -i "s/database_name_here/${MYSQL_DB}/g" wp-config-sample.php
+sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
 
 service php7.3-fpm start
