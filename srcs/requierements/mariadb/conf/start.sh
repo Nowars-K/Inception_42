75348bdd55a4871f33c6ddf5875168b5b299fd72
Alexsan <a.ferrand69@gmail.com>
2022-12-07 10:57:35 +0100

    Mariadb

--- srcs/requierements/mariadb/conf/start.sh
+++ srcs/requierements/mariadb/conf/start.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
-
 cat > setup.sql << EOF
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER aleferra@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON wordpress.* TO aleferra@'%';
 FLUSH PRIVILEGES;
+
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+
 EOF
