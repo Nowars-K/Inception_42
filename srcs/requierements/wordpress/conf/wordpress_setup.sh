5cd2dd0d9162cbe829bfd686a2e919fd727cbd84
Alexsan <a.ferrand69@gmail.com>
2022-12-08 13:44:28 +0100

    var .env use

--- srcs/requierements/wordpress/conf/wordpress_setup.sh
+++ srcs/requierements/wordpress/conf/wordpress_setup.sh
@@ -7,10 +7,10 @@ mv wordpress/ /var/www/app
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/app
-sed -i "s/username_here/$WP_USER/g" wp-config-sample.php
-sed -i "s/password_here/$WP_PASS/g" wp-config-sample.php
-sed -i "s/localhost/$WP_LOCALHOST/g" wp-config-sample.php
-sed -i "s/database_name_here/$WP_DATANAME/g" wp-config-sample.php
+sed -i "s/username_here/aleferra/g" wp-config-sample.php
+sed -i "s/password_here/pass/g" wp-config-sample.php
+sed -i "s/localhost/mariadb/g" wp-config-sample.php
+sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
 
 service php7.3-fpm start
