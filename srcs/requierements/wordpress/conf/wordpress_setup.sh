30e7a158cd6dd30f5a5f5b852b988eaebd00e6a7
Alexsan <a.ferrand69@gmail.com>
2022-12-08 17:48:22 +0100

    chg version

--- srcs/requierements/wordpress/conf/wordpress_setup.sh
+++ srcs/requierements/wordpress/conf/wordpress_setup.sh
@@ -8,7 +8,7 @@ mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/app
 sed -i "s/username_here/$WP_USER/g" wp-config-sample.php
-sed -i "s/password_here/$WP_USER/g" wp-config-sample.php
+sed -i "s/password_here/$WP_PASS/g" wp-config-sample.php
 sed -i "s/localhost/$WP_LOCALHOST/g" wp-config-sample.php
 sed -i "s/database_name_here/$WP_DATANAME/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
