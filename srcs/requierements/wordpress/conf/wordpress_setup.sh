58bddbe56c29e64e2ea11e8fd3975a171c364988
Alexsan <a.ferrand69@gmail.com>
2022-12-08 18:04:43 +0100

    sans env

--- srcs/requierements/wordpress/conf/wordpress_setup.sh
+++ srcs/requierements/wordpress/conf/wordpress_setup.sh
@@ -7,7 +7,7 @@ mv wordpress/ /var/www/app
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/app
-sed -i "s/username_here/aleferra/g" wp-config-sample.php
+sed -i "s/username_here/${WP_USER}/g" wp-config-sample.php
 sed -i "s/password_here/pass/g" wp-config-sample.php
 sed -i "s/localhost/mariadb/g" wp-config-sample.php
 sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
