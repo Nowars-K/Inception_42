d821441eb05a376734be09e315df653cfa09074a
Alexsan <a.ferrand69@gmail.com>
2022-12-09 12:04:39 +0100

    afficher env var

--- srcs/requierements/wordpress/conf/wordpress_setup.sh
+++ srcs/requierements/wordpress/conf/wordpress_setup.sh
@@ -6,8 +6,12 @@ mv wordpress/ /var/www/app
 
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 echo $WP_USER > a
+echo $WPUSER > a
+echo ${WP_USER} > a
+echo ${WPUSER} > a
+echo '$WPUSER' > a
+echo "$WP_USER" > a
 cd /var/www/app
-echo $WP_USER > a
 
 sed -i "s/username_here/'$WP_USER'/g" wp-config-sample.php
 sed -i "s/password_here/pass/g" wp-config-sample.php
